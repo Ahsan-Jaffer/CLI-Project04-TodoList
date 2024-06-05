f7f5ac394fefdcae4ba2760886761767ad53b398
Ahsan <ahsanjaffer123@gmail.com>
2024-06-05 23:25:16 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let answer = true;
+while (answer) {
+    let todosQuestion = await inquirer.prompt([
+        {
+            name: "firstQuestion",
+            type: "input",
+            message: "What would you like to add in your Todos ? "
+        },
+        {
+            name: "secondQuestion",
+            type: "confirm",
+            message: "Would you like to add more in your Todos",
+            default: "true"
+        },
+    ]);
+    let { firstQuestion, secondQuestion } = todosQuestion;
+    todos.push(firstQuestion);
+    console.log(todos);
+    answer = secondQuestion;
+}
